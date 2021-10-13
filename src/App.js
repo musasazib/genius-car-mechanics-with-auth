fb4b21a9939e43ca75889e7dcce0d70dc14452ee
musasazib <musasazib@gmail.com>
2021-10-13 18:49:12 +0600

    Added Route & 404 page

--- src/App.js
+++ src/App.js
@@ -1,10 +1,24 @@
+import { BrowserRouter as Router,Switch,Route} from 'react-router-dom';
 import './App.css';
 import Home from './pages/Home/Home/Home';
+import NotFound from './pages/NotFound/NotFound';
 
 function App() {
   return (
     <div className="App">
-      <Home></Home>
+      <Router>
+        <Switch>
+          <Route exact path="/">
+              <Home></Home>
+          </Route>
+          <Route path="/home">
+            <Home></Home>
+          </Route>
+          <Route path="*">
+            <NotFound></NotFound>
+          </Route>
+        </Switch>
+      </Router>
     </div>
   );
 }
---
+++ src/pages/NotFound/NotFound.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import notFound from '../../images/404.png'
+
+const NotFound = () => {
+    return (
+        <div>
+            <img style={{ width: '50%' }} src={notFound} alt="" />
+            <br />
+            <Link to="/"><button>Go Back</button></Link>
+        </div>
+    );
+};
+
+export default NotFound;
