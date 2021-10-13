a106d68cb4fa749c06b3bcda9f9a37579d7f3a00
musasazib <musasazib@gmail.com>
2021-10-13 18:24:58 +0600

    Added Banner

--- src/App.js
+++ src/App.js
@@ -1,10 +1,10 @@
 import './App.css';
-import Services from './pages/Home/Services/Services';
+import Home from './pages/Home/Home/Home';
 
 function App() {
   return (
     <div className="App">
-      <Services></Services>
+      <Home></Home>
     </div>
   );
 }
