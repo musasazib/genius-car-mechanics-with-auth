8dd6da48626ecc9b1742b7bef066c576891b5fbd
musasazib <musasazib@gmail.com>
2021-10-13 19:29:03 +0600

    Dynamic Route

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import { BrowserRouter as Router,Switch,Route} from 'react-router-dom';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 import './App.css';
+import Booking from './pages/Booking/Booking/Booking';
 import Home from './pages/Home/Home/Home';
 import NotFound from './pages/NotFound/NotFound';
 
@@ -9,10 +10,13 @@ function App() {
       <Router>
         <Switch>
           <Route exact path="/">
-              <Home></Home>
+            <Home></Home>
           </Route>
           <Route path="/home">
             <Home></Home>
+          </Route>
+          <Route path="/booking/:serviceId">
+            <Booking></Booking>
           </Route>
           <Route path="*">
             <NotFound></NotFound>
